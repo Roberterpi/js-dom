f78ed9a7d9da6b898304798ed10ef619044e44a9
roberterpi <robertpopct@gmail.com>
2021-11-19 18:28:31 +0200

    second commit

--- dom.js
+++ dom.js
@@ -1,1 +1,14 @@
-console.dir(document);
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = "Gigi";
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.forms);
+// console.log(document.links);
+console.log(document.images);
+
+
+
