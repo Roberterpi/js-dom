acbf19a83f122a2d4dce7e4ae9ba48d73b420aad
roberterpi <robertpopct@gmail.com>
2021-11-19 18:36:32 +0200

    third commit

--- dom.js
+++ dom.js
@@ -8,7 +8,13 @@
 // console.log(document.all);
 // console.log(document.forms);
 // console.log(document.links);
-console.log(document.images);
+// console.log(document.images);
 
+// selectors
 
-
+// console.log(document.getElementById("header-title"));
+let headerTitle = document.getElementById("header-title");
+// console.log(headerTitle);
+// headerTitle.textContent = "Hello";
+// headerTitle.innerText = "LALa";
+// headerTitle.innerHTML = "<h3>Bye</h3>"
