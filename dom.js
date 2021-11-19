9f8965372f8f3aba2bc6b924e8eb46bfc5b0bce2
roberterpi <robertpopct@gmail.com>
2021-11-19 19:22:50 +0200

    queryselectorall

--- dom.js
+++ dom.js
@@ -10,7 +10,9 @@
 // console.log(document.links);
 // console.log(document.images);
 
+
 // GETELEMENTBYID
+
 
 // console.log(document.getElementById("header-title"));
 // let headerTitle = document.getElementById("header-title");
@@ -23,6 +25,8 @@
 
 
 // GETELEMENTSBYCLASS
+
+
 // let items = document.getElementsByClassName("list-group-item");
 // console.log(items);
 // console.log(items[1]);
@@ -39,24 +43,68 @@
 //   items[i].style.backgroundColor = "grey";
 // }
 
+
 // GETELEMENTSBYTAGNAME
 
-let li = document.getElementsByTagName("li");
-console.log(li);
-console.log(li[1]);
-li[1].textContent = "Hello 1";
-li[1].style.fontWeight = "bold";
-li[1].style.backgroundColor = "yellow";
 
-// Gives error
-// items.style.backgroundColor = "grey";
+// let li = document.getElementsByTagName("li");
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = "Hello 1";
+// li[1].style.fontWeight = "bold";
+// li[1].style.backgroundColor = "yellow";
 
-// Instead we need a for loop to change the colour for all of them
+// // Gives error
+// // items.style.backgroundColor = "grey";
 
-for (let i = 0; i < li.length; i++) {
-  li[i].style.backgroundColor = "grey";
+// // Instead we need a for loop to change the colour for all of them
+
+// for (let i = 0; i < li.length; i++) {
+//   li[i].style.backgroundColor = "grey";
+// }
+
+
+// querySELECTOR
+
+
+//use it for only one item. it grabs the first one. for example if there are multiple titles it will grab only the first title
+
+// let header = document.querySelector("#main-header");
+// // @ts-ignore
+// header.style.borderBottom = "solid 10px #ccc";
+
+// let input = document.querySelector("input");
+// input.value = "Hello world";
+
+// let submit = document.querySelector ('input[type="submit"]');
+// // @ts-ignore
+// submit.value = "Send";
+
+// let item = document.querySelector(".list-group-item");
+// // @ts-ignore
+// item.style.color = "red";
+
+// let lastItem = document.querySelector(".list-group-item:last-child");
+// // @ts-ignore
+// lastItem.style.color = "blue";
+
+// let secondItem = document.querySelector(".list-group-item:nth-child(2)");
+// // @ts-ignore
+// secondItem.style.color = "green";
+
+
+// querySelectorAll
+
+
+let titles = document.querySelectorAll(".title");
+console.log(titles);
+titles[0].textContent = "Hioio";
+
+let odd = document.querySelectorAll ("li:nth-child(odd)");
+let even = document.querySelectorAll ("li:nth-child(even)");
+
+for (let i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = "purple";
+  even[i].style.backgroundColor = "orange";
 }
-
-
-
 
