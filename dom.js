065f4663913422b715794f85a19f486666e796ca
roberterpi <robertpopct@gmail.com>
2021-11-19 18:51:14 +0200

    getelementsbyclassname

--- dom.js
+++ dom.js
@@ -10,7 +10,7 @@
 // console.log(document.links);
 // console.log(document.images);
 
-// GetelementsbyID
+// GETELEMENTBYID
 
 // console.log(document.getElementById("header-title"));
 // let headerTitle = document.getElementById("header-title");
@@ -20,3 +20,27 @@
 // headerTitle.innerText = "LALa";
 // headerTitle.innerHTML = "<h3>Bye</h3>"
 // header.style.borderBottom = "solid 3px black";
+
+
+// GETELEMENTSBYCLASS
+let items = document.getElementsByClassName("list-group-item");
+console.log(items);
+console.log(items[1]);
+items[1].textContent = "Hello 1";
+items[1].style.fontWeight = "bold";
+items[1].style.backgroundColor = "yellow";
+
+// Gives error
+// items.style.backgroundColor = "grey";
+
+// Instead we need a for loop to change the colour for all of them
+
+for (let i = 0; i < items.length; i++) {
+  items[i].style.backgroundColor = "grey";
+}
+
+
+
+
+
+
