c7fc1b0a36b21678e225ef317e2f9efa4724230a
roberterpi <robertpopct@gmail.com>
2021-11-19 18:56:04 +0200

    getelementsbytagname

--- dom.js
+++ dom.js
@@ -23,23 +23,39 @@
 
 
 // GETELEMENTSBYCLASS
-let items = document.getElementsByClassName("list-group-item");
-console.log(items);
-console.log(items[1]);
-items[1].textContent = "Hello 1";
-items[1].style.fontWeight = "bold";
-items[1].style.backgroundColor = "yellow";
+// let items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = "Hello 1";
+// items[1].style.fontWeight = "bold";
+// items[1].style.backgroundColor = "yellow";
+
+// // Gives error
+// // items.style.backgroundColor = "grey";
+
+// // Instead we need a for loop to change the colour for all of them
+
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = "grey";
+// }
+
+// GETELEMENTSBYTAGNAME
+
+let li = document.getElementsByTagName("li");
+console.log(li);
+console.log(li[1]);
+li[1].textContent = "Hello 1";
+li[1].style.fontWeight = "bold";
+li[1].style.backgroundColor = "yellow";
 
 // Gives error
 // items.style.backgroundColor = "grey";
 
 // Instead we need a for loop to change the colour for all of them
 
-for (let i = 0; i < items.length; i++) {
-  items[i].style.backgroundColor = "grey";
+for (let i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = "grey";
 }
-
-
 
 
 
